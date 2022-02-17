e4e5002ca482f5f2d99eab911075c272d3bc302c
sbothra2 <sbothra2000@gmail.com>
2022-02-17 23:13:00 +0530

    Add project files.

---
+++ Shubhansh/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+public class myMainClass
+{
+    public static void Main()
+    {
+        DuplicateFinderLoop obj = new DuplicateFinderLoop();
+        Console.WriteLine("Enter a string to find Duplicates: ");
+        var inputString = Console.ReadLine();
+        obj.findDuplicate(inputString);
+        Console.ReadLine();
+    }
+}
---
+++ Shubhansh/DuplicateFinderLoop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class DuplicateFinderLoop
+{
+    internal void findDuplicate(string rcvdString)
+    {
+        var str = rcvdString.ToCharArray();
+        int count = 0;
+        Console.WriteLine("the duplicates in " + rcvdString + " are: ");
+        for (int i = 0; i < str.Length; i++)
+        {
+            count = 0;
+            for (int j = i + 1; j < str.Length; j++)
+            {
+                if (str[i] == str[j] && str[i] != ' ')
+                {
+                    count++;
+                    str[j] = '`';
+                }
+            }
+            if (count > 0 && str[i] != '`')
+                Console.Write(str[i] + " ");
+        }
+    }
+}
